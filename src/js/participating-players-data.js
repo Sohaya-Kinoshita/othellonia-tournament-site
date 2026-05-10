fb3bd44c4c41000c4631825cd672248a5e5b9321
木下素早 <e1b23025@oit.ac.jp>
2026-05-11 00:02:30 +0900

    feat: 参加チームデータのサブリーダー名を追加し、醤油ラーメンの表記を修正

--- src/js/participating-players-data.js
+++ src/js/participating-players-data.js
@@ -55,11 +55,6 @@ window.participatingPlayers = [
     team_name_2: "",
   },
   {
-    player_name: "あびｽ",
-    team_name_1: "塩ラーメン",
-    team_name_2: "",
-  },
-  {
     player_name: "のえむ",
     team_name_1: "塩ラーメン",
     team_name_2: "",
@@ -80,11 +75,6 @@ window.participatingPlayers = [
     team_name_2: "",
   },
   {
-    player_name: "はむかつ",
-    team_name_1: "塩ラーメン",
-    team_name_2: "",
-  },
-  {
     player_name: "シノ",
     team_name_1: "塩ラーメン",
     team_name_2: "",
@@ -101,82 +91,67 @@ window.participatingPlayers = [
   },
   {
     player_name: "KOU＠",
-    team_name_1: "醬油ラーメン",
+    team_name_1: "醤油ラーメン",
     team_name_2: "",
   },
   {
     player_name: "ユーユー",
-    team_name_1: "醬油ラーメン",
+    team_name_1: "醤油ラーメン",
     team_name_2: "",
   },
   {
     player_name: "キドココ",
-    team_name_1: "醬油ラーメン",
-    team_name_2: "",
-  },
-  {
-    player_name: "テトラポット",
-    team_name_1: "醬油ラーメン",
+    team_name_1: "醤油ラーメン",
     team_name_2: "",
   },
   {
     player_name: "LITO",
-    team_name_1: "醬油ラーメン",
-    team_name_2: "",
-  },
-  {
-    player_name: "ぽぽぽぽ",
-    team_name_1: "醬油ラーメン",
+    team_name_1: "醤油ラーメン",
     team_name_2: "",
   },
   {
     player_name: "Ke-dai",
-    team_name_1: "醬油ラーメン",
+    team_name_1: "醤油ラーメン",
     team_name_2: "",
   },
   {
     player_name: "しおから！",
-    team_name_1: "醬油ラーメン",
+    team_name_1: "醤油ラーメン",
     team_name_2: "",
   },
   {
     player_name: "あまもち",
-    team_name_1: "醬油ラーメン",
+    team_name_1: "醤油ラーメン",
     team_name_2: "",
   },
   {
     player_name: "まるあゆ",
-    team_name_1: "醬油ラーメン",
+    team_name_1: "醤油ラーメン",
     team_name_2: "",
   },
   {
     player_name: "mesty",
-    team_name_1: "醬油ラーメン",
+    team_name_1: "醤油ラーメン",
     team_name_2: "",
   },
   {
     player_name: "速攻元気",
-    team_name_1: "醬油ラーメン",
-    team_name_2: "",
-  },
-  {
-    player_name: "しろ",
-    team_name_1: "醬油ラーメン",
+    team_name_1: "醤油ラーメン",
     team_name_2: "",
   },
   {
     player_name: "KY＠罠リペア愛好家",
-    team_name_1: "醬油ラーメン",
+    team_name_1: "醤油ラーメン",
     team_name_2: "",
   },
   {
     player_name: "きし",
-    team_name_1: "醬油ラーメン",
+    team_name_1: "醤油ラーメン",
     team_name_2: "",
   },
   {
     player_name: "くるるぎ",
-    team_name_1: "醬油ラーメン",
+    team_name_1: "醤油ラーメン",
     team_name_2: "",
   },
   {
@@ -186,11 +161,6 @@ window.participatingPlayers = [
   },
   {
     player_name: "Synon（シノン）",
-    team_name_1: "豚骨ラーメン",
-    team_name_2: "",
-  },
-  {
-    player_name: "栞",
     team_name_1: "豚骨ラーメン",
     team_name_2: "",
   },
@@ -210,22 +180,12 @@ window.participatingPlayers = [
     team_name_2: "",
   },
   {
-    player_name: "リゼ",
-    team_name_1: "豚骨ラーメン",
-    team_name_2: "",
-  },
-  {
     player_name: "サブリー",
     team_name_1: "豚骨ラーメン",
     team_name_2: "",
   },
   {
     player_name: "れお",
-    team_name_1: "豚骨ラーメン",
-    team_name_2: "",
-  },
-  {
-    player_name: "なっくす",
     team_name_1: "豚骨ラーメン",
     team_name_2: "",
   },
--- src/js/participating-teams-data.js
+++ src/js/participating-teams-data.js
@@ -2,13 +2,15 @@ window.participatingTeams = [
   {
     team_name: "味噌ラーメン",
     leader_name: "こーだゐ",
+    subleader_name: "愛瑠",
   },
   {
     team_name: "塩ラーメン",
     leader_name: "れおん",
+    subleader_name: "Nの人",
   },
   {
-    team_name: "醬油ラーメン",
+    team_name: "醤油ラーメン",
     leader_name: "KOU＠",
   },
   {
