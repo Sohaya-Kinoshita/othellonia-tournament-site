69d616d98b32e8356a465487dc5f27de54f3fa29
木下素早 <e1b23025@oit.ac.jp>
2026-05-14 23:32:22 +0900

    feat: 参加チームの名前を更新し、チーム情報を改善

--- src/js/participating-teams-data.js
+++ src/js/participating-teams-data.js
@@ -1,21 +1,21 @@
 window.participatingTeams = [
   {
-    team_name: "味噌ラーメン",
+    team_name: "祝みそきん新店舗",
     leader_name: "こーだゐ",
     subleader_name: "JTA",
   },
   {
-    team_name: "塩ラーメン",
+    team_name: "塩麺極道",
     leader_name: "れおん",
     subleader_name: "Nの人",
   },
   {
-    team_name: "醤油ラーメン",
+    team_name: "醤油しか勝たん",
     leader_name: "しの@志天",
     subleader_name: "LITO",
   },
   {
-    team_name: "豚骨ラーメン",
+    team_name: "愛瑠くん以外バリカタ",
     leader_name: "たらちゃん",
     subleader_name: "愛瑠",
   },
